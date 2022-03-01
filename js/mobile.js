1e7c888634dfb94337d607214b8c391637536ca2
Akash Zaman <96652000+AkasHZamaN@users.noreply.github.com>
2022-03-02 03:24:07 +0600

    style added

--- js/mobile.js
+++ js/mobile.js
@@ -35,13 +35,13 @@ const displaySearchResult = data => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card h-100 rounded">
+        <div class="card h-100 border-0 rounded-3">
             <img src="${data.image}" class="card-img-top w-75 mx-auto p-3" alt="...">
             <div class="card-body">
-                <h5 class="card-title">${data.brand}</h5>
-                <h6>${data.phone_name}</h6>
-                <p>${data.slug}</p>
-                <button onclick="mobileDetails('${data.slug}')" type="button" class="btn btn-success">Explore</button>
+                <h5 class="card-title fw-bold">${data.brand}</h5>
+                <h2>${data.phone_name}</h2>
+                <p class="card-title">${data.slug}</p>
+                <button onclick="mobileDetails('${data.slug}')" type="button" class="btn btn-success fw-bold px-3">Explore</button>
             </div>
         </div>
         `;
@@ -59,7 +59,7 @@ const mobileDetails = mobileId => {
     .then(res => res.json())
     .then(data => displayMobileDetails(data.data));
 }
-    // creat card and display more information
+    // create card and display more information
     const displayMobileDetails = mobile => {
     console.log(mobile);
     const mobileDetail = document.getElementById('mobile-detail');
@@ -69,24 +69,30 @@ const mobileDetails = mobileId => {
     <img src="${mobile.image}" class="card-img-top w-75 p-3 mx-auto" alt="...">
     <div class="card-body">
         <h5 class="card-title fw-bold">${mobile.brand}</h5>
-        <p>${mobile.slug}</p>
+        <p class="card-title fs-4">${mobile.slug}</p>
 
-        <h6 class="card-title fw-bold">${mobile.name}</h6>
+        <h6 class="card-title fw-bold fs-3">${mobile.name}</h6>
         <p></p>
-        <h6 class="card-title"><span class="fw-bold">Released:</span> ${mobile.releaseDate ? mobile.releaseDate : 'Not Found!'}</h6>
-        <h6 class="card-title fw-bold">Main Features:</h6>
-        <h6 class="card-title"><span class="fw-bold">chipSet:</span> ${mobile.mainFeatures.chipSet}</h6>
-        <h6 class="card-title"><span class="fw-bold">display:</span> ${mobile.mainFeatures.displaySize}</h6>
-        <h6 class="card-title"><span class="fw-bold">memory:</span> ${mobile.mainFeatures.memory}</h6>
-        <h6 class="card-title"><span class="fw-bold">storage:</span> ${mobile.mainFeatures.storage}</h6>
-        <h6 class="card-title"><span class="fw-bold">sensors:</span> ${mobile.mainFeatures.sensors}</h6>
-        <h6 class="card-title fw-bold">Others:</h6>
-        <h6 class="card-title"><span class="fw-bold">Bluetooth:</span> ${mobile.others?.Bluetooth ? mobile.others?.Bluetooth : 'Not Found!'}</h6>
-        <h6 class="card-title"><span class="fw-bold">GPS:</span> ${mobile.others?.GPS ? mobile.others?.GPS : 'Not Found!'}</h6>
-        <h6 class="card-title"><span class="fw-bold">NFC:</span> ${mobile.others?.NFC ? mobile.others?.NFC : 'Not Found!'}</h6>
-        <h6 class="card-title"><span class="fw-bold">Radio:</span> ${mobile.others?.Radio ? mobile.others?.Radio: 'Not Found!'}</h6>
-        <h6 class="card-title"><span class="fw-bold">USB:</span> ${mobile.others?.USB ? mobile.others?.USB : 'Not Found!'}</h6>
-        <h6 class="card-title"><span class="fw-bold">WLAN:</span> ${mobile.others?.WLAN ? mobile.others?.WLAN : 'Not Found!'}</h6>
+        <h6 class="card-title fs-5"><span class="fw-bold fs-5">Released:</span> ${mobile.releaseDate ? mobile.releaseDate : 'Not Found!'}</h6>
+
+        <hr class="w-75 mx-auto border border-1 border-success opacity-75 rounded">
+
+        <h6 class="card-title fw-bold fs-5">Main Features:</h6>
+        <h6 class="card-title fs-5"><span class="fw-bold fs-5">chipSet:</span> ${mobile.mainFeatures.chipSet}</h6>
+        <h6 class="card-title fs-5"><span class="fw-bold fs-5">display:</span> ${mobile.mainFeatures.displaySize}</h6>
+        <h6 class="card-title fs-5"><span class="fw-bold fs-5">memory:</span> ${mobile.mainFeatures.memory}</h6>
+        <h6 class="card-title fs-5"><span class="fw-bold fs-5">storage:</span> ${mobile.mainFeatures.storage}</h6>
+        <h6 class="card-title fs-5"><span class="fw-bold fs-5">sensors:</span> ${mobile.mainFeatures.sensors}</h6>
+
+        <hr class="w-75 mx-auto border border-1 border-success opacity-75 rounded">
+
+        <h6 class="card-title fw-bold fs-5">Others:</h6>
+        <h6 class="card-title fs-5"><span class="fw-bold fs-5">Bluetooth:</span> ${mobile.others?.Bluetooth ? mobile.others?.Bluetooth : 'Not Found!'}</h6>
+        <h6 class="card-title fs-5"><span class="fw-bold fs-5">GPS:</span> ${mobile.others?.GPS ? mobile.others?.GPS : 'Not Found!'}</h6>
+        <h6 class="card-title fs-5"><span class="fw-bold fs-5">NFC:</span> ${mobile.others?.NFC ? mobile.others?.NFC : 'Not Found!'}</h6>
+        <h6 class="card-title fs-5"><span class="fw-bold fs-5">Radio:</span> ${mobile.others?.Radio ? mobile.others?.Radio: 'Not Found!'}</h6>
+        <h6 class="card-title fs-5"><span class="fw-bold fs-5">USB:</span> ${mobile.others?.USB ? mobile.others?.USB : 'Not Found!'}</h6>
+        <h6 class="card-title fs-5"><span class="fw-bold fs-5">WLAN:</span> ${mobile.others?.WLAN ? mobile.others?.WLAN : 'Not Found!'}</h6>
 
     </div>
     `;
