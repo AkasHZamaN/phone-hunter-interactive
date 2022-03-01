04cf4ccde070c145a92499aa9f964cfc4fe3bdea
Akash Zaman <96652000+AkasHZamaN@users.noreply.github.com>
2022-03-01 21:40:45 +0600

    fifth commit

--- js/mobile.js
+++ js/mobile.js
@@ -14,18 +14,19 @@ const searchMobile = () => {
 
 const displaySearchResult = data => {
     const searchResult = document.getElementById('search-result');
+        searchResult.innerHTML = '';
     data.forEach(data => {
-        console.log(data);
+        // console.log(data);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100">
-            <img src="${data.image}" class="card-img-top h-75" alt="...">
+            <img src="${data.image}" class="card-img-top w-75 mx-auto p-3" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${data.brand}</h5>
                 <h6>${data.phone_name}</h6>
                 <p>${data.slug}</p>
-                <button type="button" class="btn btn-success">Explore</button>
+                <button onclick="mobileDetails('${data.slug}')" type="button" class="btn btn-success">Explore</button>
             </div>
         </div>
         `;
@@ -35,5 +36,44 @@ const displaySearchResult = data => {
 
 // mobile details information
 const mobileDetails = mobileId => {
-    console.log(mobileId);
+    const url = `https://openapi.programming-hero.com/api/phone/${mobileId}`;
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayMobileDetails(data.data));
+}
+    // creat card and display more information
+    const displayMobileDetails = mobile => {
+        console.log(mobile);
+    const mobileDetail = document.getElementById('mobile-detail');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img src="${mobile.image}" class="card-img-top w-75 p-3 mx-auto" alt="...">
+    <div class="card-body">
+        <h5 class="card-title fw-bold">${mobile.brand}</h5>
+        <p>${mobile.slug}</p>
+
+        <h6 class="card-title fw-bold">${mobile.name}</h6>
+        <p></p>
+        <h6 class="card-title"><span class="fw-bold">Released:</span> ${mobile.releaseDate ? mobile.releaseDate : 'Not Found!'}</h6>
+        <h6 class="card-title fw-bold">Main Features:</h6>
+        <h6 class="card-title"><span class="fw-bold">chipSet:</span> ${mobile.mainFeatures.chipSet}</h6>
+        <h6 class="card-title"><span class="fw-bold">display:</span> ${mobile.mainFeatures.displaySize}</h6>
+        <h6 class="card-title"><span class="fw-bold">memory:</span> ${mobile.mainFeatures.memory}</h6>
+        <h6 class="card-title"><span class="fw-bold">storage:</span> ${mobile.mainFeatures.storage}</h6>
+        <h6 class="card-title"><span class="fw-bold">sensors:</span> ${mobile.mainFeatures.sensors}</h6>
+        <h6 class="card-title fw-bold">Others:</h6>
+        <h6 class="card-title"><span class="fw-bold">Bluetooth:</span> ${mobile.others?.Bluetooth ? mobile.others?.Bluetooth : 'Not Found!'}</h6>
+        <h6 class="card-title"><span class="fw-bold">GPS:</span> ${mobile.others?.GPS ? mobile.others?.GPS : 'Not Found!'}</h6>
+        <h6 class="card-title"><span class="fw-bold">NFC:</span> ${mobile.others?.NFC ? mobile.others?.NFC : 'Not Found!'}</h6>
+        <h6 class="card-title"><span class="fw-bold">Radio:</span> ${mobile.others?.Radio ? mobile.others?.Radio: 'Not Found!'}</h6>
+        <h6 class="card-title"><span class="fw-bold">USB:</span> ${mobile.others?.USB ? mobile.others?.USB : 'Not Found!'}</h6>
+        <h6 class="card-title"><span class="fw-bold">WLAN:</span> ${mobile.others?.WLAN ? mobile.others?.WLAN : 'Not Found!'}</h6>
+
+    </div>
+    `;
+        // clear previous result after new explore
+    mobileDetail.innerHTML = '';
+    mobileDetail.appendChild(div);
 }
